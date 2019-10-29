0598d629bd003ec6395008519f70cea8a84af8bd
saptc07 <46454646+saptc07@users.noreply.github.com>
2019-10-29 21:27:08 +0530

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,35 @@
+ 
+
+library(shiny)
+
+ 
+shinyUI(fluidPage(
+
+     
+    titlePanel("first app design"),
+
+     
+    sidebarLayout(
+        sidebarPanel(
+            h4("creating user interface"),
+            sliderInput("slider",0,50,0)
+             
+        ),
+
+         
+        mainPanel(
+            h3("main panel user interface")
+            
+            
+
+            
+            
+            
+        )
+    )get_prod<- reactive({
+        input1+input2
+        
+        get_prod()
+)
+        
+        })
